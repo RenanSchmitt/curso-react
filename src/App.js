d1fb7206d454ff8b1c8246db8e46256d6e63d525
RenanSchmitt <renanschmitt.ti@gmail.com>
2020-03-12 17:37:33 -0300

    Add the project

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import './App.css';
+import Tabela from './Tabela'
+
+class App extends Component {
+
+    state = {
+      autores: [
+      {
+        nome: 'Paulo',
+        livro: 'React',
+        preco: '1000'
+      },
+      {
+        nome: 'Daniel',
+        livro: 'Java',
+        preco: '99'
+      },
+      {
+        nome: 'Marcos',
+        livro: 'Design',
+        preco: '150'
+      },
+      {
+        nome: 'Bruno',
+        livro: 'DevOps',
+        preco: '100'
+      }
+    ],
+  };
+
+  removeAutor = index => {
+    
+    const { autores } = this.state;
+
+    this.setState(
+      {
+        autores : autores.filter((autor, posAtual) => {
+          return posAtual !== index;
+        }),
+      }
+    );
+  }
+
+    render(){
+      return (
+        <div className="App">
+          <Tabela autores = { this.state.autores } removeAutor = { this.removeAutor  }/>
+        </div>
+      );
+    }
+}
+
+export default App;
