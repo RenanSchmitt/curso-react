11adf2b83693316d3602443c88595ffc16b7e307
RenanSchmitt <renanschmitt.ti@gmail.com>
2020-03-16 16:20:16 -0300

    add toast

--- src/App.js
+++ src/App.js
@@ -1,46 +1,67 @@
 import React, { Component, Fragment } from 'react';
-import 'materialize-css/dist/css/materialize.min.css';
-import './App.css';
+import 'materialize-css/dist/css/materialize.min.css'
 import Tabela from './Tabela';
-import Form from './Formulario';
+import Formulario from './Formulario';
 import Header from './Header';
+import PopUp from './PopUp';
+
 
 class App extends Component {
 
-    state = {
-      autores: [
-
+  state = {
+    autores: [
+      {
+        nome: 'Paulo',
+        livro: 'React',
+        preco: '1000'
+      },
+      {
+        nome: 'Daniel',
+        livro: 'Java',
+        preco: '99'
+      },
+      {
+        nome: 'Marcos',
+        livro: 'Design',
+        preco: '150'
+      },
+      {
+        nome: 'Bruno',
+        livro: 'DevOps',
+        preco: '100'
+      }
     ],
-  };
+  }
 
   removeAutor = index => {
-    
+
     const { autores } = this.state;
 
-    this.setState(
-      {
-        autores : autores.filter((autor, posAtual) => {
-          return posAtual !== index;
-        }),
-      }
-    );
+    this.setState({
+      autores: autores.filter((autor, posAtual) => {
+        return posAtual !== index;
+        
+      }),
+    })
+    PopUp.exibeMensagem("sucess", "Publicação removida com sucesso.")
   }
 
   escutadorDeSubmit = autor => {
-    this.setState({ autores:[...this.state.autores, autor]})
+    this.setState({ autores: [...this.state.autores, autor] });
+    PopUp.exibeMensagem("sucess", "Publicação adicionada com sucesso.")
   }
-
-    render(){
-      return (
-        <Fragment>
-          <Header />
-          <div className="container mb-10">
-          <Tabela autores = { this.state.autores } removeAutor = { this.removeAutor  }/>
-          <Form escutadorDeSubmit = {this.escutadorDeSubmit}/>
-          </div>
-        </Fragment>
-      );
-    }
+  render() {
+    return (
+      <Fragment>
+        <Header />
+        <div className="container mb-10">
+          <h1>Casa do código</h1>
+          <Tabela autores={this.state.autores} removeAutor={this.removeAutor} />
+          <Formulario escutadorDeSubmit={this.escutadorDeSubmit} />
+        </div>
+      </Fragment>
+    );
+  }
 }
 
 export default App;
--- src/Header.js
+++ src/Header.js
@@ -1,17 +1,17 @@
-import React from 'react'
+import React from 'react';
 
-const Header = () => {
-    return(
-<nav>
-<div className="nav-wrapper indigo lighten-2">
-  <a href="/" className="brand-logo ">Casa do Código</a>
-  <ul id="nav-mobile" className="right hide-on-med-and-down">
-    <li><a href="/autores">Autores</a></li>
-    <li><a href="/livros">Livros</a></li>
-    <li><a href="/sobre">Sobre   </a></li>
-  </ul>
-</div>
-</nav>
-)};
-
+const Header = () =>{
+    return (
+        <nav>
+                <div className="nav-wrapper indigo lighten-2">
+                    <a href="/" className="brand-logo ml-3">Casa do Código</a>
+                    <ul id="nav-mobile" className="right">
+                        <li><a href="/autores">Autores</a></li>
+                        <li><a href="/livros">Livros</a></li>
+                        <li><a href="/sobre">Sobre</a></li>
+                    </ul>
+                </div>
+            </nav>
+    );
+}
 export default Header;
