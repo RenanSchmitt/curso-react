23daff538539098e774c675a7e58c1b0df10b08a
RenanSchmitt <renanschmitt.ti@gmail.com>
2020-03-16 18:07:52 -0300

    Adiciona rotas no projeto

---
+++ src/Autores.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+import Header from './Header';
+
+const Autores = () => {
+    return(
+        <Fragment>
+            < Header/>
+        </Fragment>
+
+    );
+}
+
+export default Autores;
